e783cc01d5428634d5fe1a6a2a7931bdad2756c0
huntercuneo1 <huntercuneo1@gmail.com>
2026-04-09 15:37:41 -0700

    Update twilio-token.js

--- api/twilio-token.js
+++ api/twilio-token.js
@@ -1,10 +1,6 @@
-const crypto = require('crypto');
-
-function base64url(obj) {
-  const str = typeof obj === 'string' ? obj : JSON.stringify(obj);
-  return Buffer.from(str).toString('base64')
-    .replace(/=/g,'').replace(/\+/g,'-').replace(/\//g,'_');
-}
+const twilio = require('twilio');
+const AccessToken = twilio.jwt.AccessToken;
+const VoiceGrant = AccessToken.VoiceGrant;
 
 module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,39 +10,24 @@ module.exports = function handler(req, res) {
   if (req.method === 'OPTIONS') { res.status(200).end(); return; }
 
   try {
-    const accountSid  = 'ACe63e8527401fc9035a5eeefdda09a979';
-    const authToken   = 'ff5f897fd27a4889a484923d8e022dfa';
-    const appSid      = 'AP1ec734d127905ed29a7946f7da1e9805';
-    const identity    = 'hunter';
-    const now         = Math.floor(Date.now() / 1000);
+    const accountSid   = 'ACe63e8527401fc9035a5eeefdda09a979';
+    const apiKeySid    = 'SK51764bb9dcc7aad5fe3d18125085ab40';
+    const apiKeySecret = 'QNlKvZbliC9QBBtqRwZo15nRio9spq15';
+    const appSid       = 'AP1ec734d127905ed29a7946f7da1e9805';
 
-    // Twilio Access Token format
-    const header = base64url({ cty: 'twilio-fpa;v=1', typ: 'JWT', alg: 'HS256' });
-    
-    const payload = base64url({
-      jti: `${accountSid}-${now}`,
-      iss: accountSid,
-      sub: accountSid,
-      iat: now,
-      exp: now + 3600,
-      grants: {
-        identity: identity,
-        voice: {
-          outgoing: { application_sid: appSid },
-          incoming: { allow: false }
-        }
-      }
+    const token = new AccessToken(accountSid, apiKeySid, apiKeySecret, {
+      identity: 'hunter',
+      ttl: 3600
     });
 
-    const unsigned = `${header}.${payload}`;
-    const sig = crypto
-      .createHmac('sha256', authToken)
-      .update(unsigned)
-      .digest('base64')
-      .replace(/=/g,'').replace(/\+/g,'-').replace(/\//g,'_');
+    const voiceGrant = new VoiceGrant({
+      outgoingApplicationSid: appSid,
+      incomingAllow: false
+    });
 
-    const token = `${unsigned}.${sig}`;
-    res.status(200).json({ token, identity });
+    token.addGrant(voiceGrant);
+
+    res.status(200).json({ token: token.toJwt(), identity: 'hunter' });
   } catch(err) {
     console.error(err);
     res.status(500).json({ error: err.message });
